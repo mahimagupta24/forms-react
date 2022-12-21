7b360fd68c45098c618c2779f64aff4363c1bd68
mahimagupta24 <mahimagupta2401@gmail.com>
2022-12-21 15:51:21 +0530

    refactor

--- src/SimpleInput.js
+++ src/SimpleInput.js
@@ -1,65 +1,54 @@
-import React, { useEffect,useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const SimpleInput = (props) => {
-    //  const nameInputRef = useRef()
+  //  const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState("");
-  const[enteredNameIsValid,setEnteredNameIsValid] = useState(false);
-  const[enteredNameIsTouched,setEnteredNameIsTouched]=useState(false);
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
 
-  useEffect(()=>{
-    if(enteredNameIsValid){
-        console.log('valid')
-    }
-  },[enteredNameIsValid])
+  const enteredNameIsValid = enteredName !== "";
+  const enteredNameIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+  
 
   const inputNameHandler = (event) => {
     setEnteredName(event.target.value);
-    
-    if(event.target.value!==""){
-        setEnteredNameIsValid(true)
-    }
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    setEnteredNameIsTouched(true)
-
-if(enteredName===""){
-    setEnteredNameIsValid(false)
-    return;
-}
-setEnteredNameIsValid(true)
-
- //  const enteredValue = nameInputRef.current.value;
-    // console.log(enteredValue);
-    //  setEnteredName('')
-  }
-  const enteredNameIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
-  const inputNameClasses = enteredNameIsInvalid ? 'form-control invalid': 'form-control';
-
-  
-
-  const inputNameBlurHandler=()=>{
     setEnteredNameIsTouched(true);
-    if(enteredName===""){
-        setEnteredNameIsValid(false)
+    if(!enteredNameIsValid){
+      return;
     }
-}
+
+    //  const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+      setEnteredName('')
+      setEnteredNameIsTouched(false)
+  };
+  
+  const inputNameClasses = enteredNameIsInvalid
+  ? "form-control invalid"
+  : "form-control";
+  const inputNameBlurHandler = () => {
+    setEnteredNameIsTouched(true);
+  };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-        {/* <div className="form-control"> */}
-      <div className = {inputNameClasses}>
+      {/* <div className="form-control"> */}
+      <div className={inputNameClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-        //    ref = {nameInputRef}
+          //    ref = {nameInputRef}
           type="text"
           id="name"
           onChange={inputNameHandler}
           onBlur={inputNameBlurHandler}
           value={enteredName}
         />
-       {enteredNameIsInvalid&&<p className="error-text">Entered name is invalid</p>}
+        {enteredNameIsInvalid && (
+          <p className="error-text">Entered name is invalid</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
