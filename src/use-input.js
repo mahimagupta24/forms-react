edbd6d6972597205bc1ec5e042eb9928bdff69b2
mahimagupta24 <mahimagupta2401@gmail.com>
2022-12-23 15:30:56 +0530

    new component added

--- src/use-input.js
+++ src/use-input.js
@@ -1,25 +1,31 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-const useInput=(validateValue)=>{
-    const [enteredValue, setEnteredValue] = useState("");
-    const [isTouched, setIsTouched] = useState(false);
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
 
-    const valueIsValid = validateValue(enteredValue)
+  const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
 
   const valueChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-const inputBlurHandler = () => {
+  const inputBlurHandler = () => {
     setIsTouched(true);
-}
-return {
-    value : enteredValue,
-    hasError,
-    valueChangeHandler,
-    inputBlurHandler
-
-}
   };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+  return {
+    value: enteredValue,
+    hasError,
+    isValid: valueIsValid,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 export default useInput;
